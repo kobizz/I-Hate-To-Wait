4dec542a11caf8d10f8525b6ba7139e90e5cca26
cobi zaltzberg <jzaltzberg@gmail.com>
2015-03-05 00:17:27 +0200

    adding site to supported sites

--- js/supportedSites.js
+++ js/supportedSites.js
@@ -1,6 +1,7 @@
 ImmediatelyUnlock.supportedSites = {
 	'f2h.nana10.co.il': 'f2h',
 	'allmyvideos.net': 'allmyvideos',
+	'vidspot.net': 'allmyvideos',
 	'www.upf.co.il': 'upf',
 	'short.koshare.me': 'koshare'
 };
